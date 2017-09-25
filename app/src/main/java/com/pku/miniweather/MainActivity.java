89170ec950f633b2859e4198e293f811795c8125
tammy <2995518577@qq.com>
2017-09-25 21:03:04 +0800

    init project

--- app/src/main/java/com/pku/miniweather/MainActivity.java
+++ app/src/main/java/com/pku/miniweather/MainActivity.java
@@ -1,9 +1,10 @@
 package com.pku.miniweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
